d6f06e5c75d7becf3f81d4949438460fcc874776
RalXYZ <ranunculz@zju.edu.cn>
2020-12-18 20:18:18 +0800

    Add Organization API related to database and database.go Add create, query, update methods of Organization API

--- database/organization.go
+++ database/organization.go
@@ -1,10 +1,49 @@
 package database
 
-import "time" // TODO: move this another place (RalXYZ)
+import (
+	"errors"
+	"gorm.io/gorm"
+	"time"
+)
 
 type Organization struct {
-	ID          uint      `gorm:"not_null;auto_increment;primary_key"`
-	Name        string    `gorm:"size:40;not_null"`
+	ID          uint      `gorm:"not null;autoIncrement;primaryKey"`
+	Name        string    `gorm:"size:40;not null"`
 	Description string    `gorm:"size:200"`
-	UpdateTime  time.Time `gorm:"not_null"`
+	UpdateTime  time.Time `gorm:"not null"`
+}
+
+func createOrganization(requestOrganization *Organization) error {
+	/*
+		if err := DB.First(&Organization{}, "name = ?", requestOrganization.Name).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+			DB.Create(requestOrganization)
+			return nil
+		} else {
+			return errors.New("user exists")
+		}
+	*/
+	DB.Create(requestOrganization)
+	return nil
+}
+
+func queryOrganization(ID uint) *Organization {
+	var result Organization
+	if err := DB.First(&result, "ID = ?", ID).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil
+	} else {
+		return &result
+	}
+}
+
+func updateOrganization(requestOrganization *Organization) error {
+	var result Organization
+	if err := DB.First(&result, "name = ?", requestOrganization.Name).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.New("user not found")
+	} else {
+		if result := DB.Model(&result).Updates(requestOrganization); result.Error != nil {
+			return result.Error
+		} else {
+			return nil
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.zjuqsc.com/rop/rop-back-neo
 
 go 1.15
+
+require gorm.io/gorm v1.20.8
