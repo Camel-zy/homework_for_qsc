80f9c11f3411777235600ee6de8f7458dec496c7
OE.Heart <2535360062@qq.com>
2020-12-19 11:27:44 +0800

    add Event APIs related to database

---
+++ database/event.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"git.zjuqsc.com/rop/rop-back-neo/database/model"
+	"git.zjuqsc.com/rop/rop-back-neo/database/proto"
+)
+
+func CreateEvent(requestEvent *model.Event) error {
+	return proto.Create(requestEvent)
+}
+
+func QueryEvent(ID uint) (*model.Event, error) {
+	var dbEvent model.Event
+	if result := DB.First(&dbEvent, "id = ?", ID); result.Error != nil {
+		return nil, result.Error
+	} else {
+		return &dbEvent, nil
+	}
+}
+
+func UpdateEvent(requestEvent *model.Event) error {
+	var dbEvent model.Event
+	if result := DB.First(&dbEvent, "name = ?", requestEvent.Name); result.Error != nil {
+		return result.Error
+	} else {
+		if result := DB.Model(&dbEvent).Updates(requestEvent); result.Error != nil {
+			return result.Error
+		} else {
+			return nil
+		}
+	}
+}
