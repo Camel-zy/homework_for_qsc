37be45d434b0498ec23d6be16dd8b705c9763ca8
RalXYZ <ranunculz@zju.edu.cn>
2021-01-03 02:05:06 +0800

    Fix some bug in JWT auth function

--- web/auth/auth.go
+++ web/auth/auth.go
@@ -7,33 +7,37 @@ import (
 	"time"
 )
 
-// the functions in this file are currently unused,
-// and haven't been tested.
+// the functions in this file are currently unused.
 
-func GenerateJWT() (string, *time.Time) {
-	secretKey := viper.GetString("jwt.secret_key")
+func GenerateJWT(uid string) (string, *time.Time) {
+	mySigningKey := []byte(viper.GetString("jwt.secret_key"))
+
 	maxAge := 60 * 10     // 10 minuets
 	expireTime := time.Now().Add(time.Duration(maxAge) * time.Second)
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.StandardClaims{ExpiresAt: expireTime.Unix(), Issuer: "rop"})
-	tokenString, err := token.SignedString([]byte(secretKey))
+
+	claims := &jwt.StandardClaims{
+		ExpiresAt: expireTime.Unix(),
+		Issuer:    "rop",
+		Subject:   uid,      // FIXME: Encrypt this!!!
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(mySigningKey)
 	if err != nil {
 		panic(err)
 	}
 	return tokenString, &expireTime
 }
 
-func ParseJWT(tokenString string) (*jwt.StandardClaims, error) {
-	secretKey := viper.GetString("jwt.secret_key")
+func ParseJWT(tokenString string) *jwt.Token {
+	mySigningKey := []byte(viper.GetString("jwt.secret_key"))
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpexted singing method: %v\n", token.Header["alg"])
-		} else {
-			return []byte(secretKey), nil
 		}
+		return mySigningKey, nil
 	})
-	if claims, ok := token.Claims.(jwt.StandardClaims); ok && token.Valid {
-		return &claims, nil
-	} else {
-		return nil, err
+	if err != nil {
+		panic(err)
 	}
+	return token
 }
