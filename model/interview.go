b21e6451c3c99696a5f0534d75638f77bae0d52d
TO/GA <chenyi2000@zju.edu.cn>
2021-02-25 16:59:58 +0800

    seperate interview and interviewee to two files

--- model/interview.go
+++ model/interview.go
@@ -52,31 +52,6 @@ type InterviewResponse struct {
 	EndTime        time.Time
 }
 
-type JoinedInterview struct {
-	ID            uint      `gorm:"not null;autoIncrement;primaryKey"`
-	InterviewID   uint      `gorm:"not null"`
-	IntervieweeID uint      `gorm:"not null"`
-	Result        uint      `gorm:"default:0"`
-	UpdatedTime   time.Time `gorm:"not null"`
-}
-
-// 一个志愿一条记录
-type Interviewee struct {
-	ID               uint           `gorm:"not null;autoIncrement;primaryKey"`
-	EventID          uint           `gorm:"not null"`
-	AnswerID         uint           `gorm:"not null"`
-	DepartmentID     uint           `gorm:"not null"`           // 志愿部门
-	IntentRank       uint           `gorm:"not null;default:0"` // 第几志愿
-	Round            uint           `gorm:"not null;default:1"` // 公海为1，一面为2，以此类推
-	SentMessage      uint           `gorm:"not null;default:1"` // 发送过选择面试场次短信的为2，没有为1
-	InterviewOptions datatypes.JSON // 发送选择面试场次的短信用
-	Status           uint           `gorm:"not null; default:1"` // 1 本轮接受但还没选择下轮面试时间，2 面试进行中，3 纳入组织，4 拒绝
-}
-
-type IntervieweeRequest struct {
-	InterviewOptions []uint `json:"InterviewOptions"`
-}
-
 func CreateInterview(interviewRequest *InterviewRequest, eid uint, did uint, rnd uint) (uint, error) {
 	dbInterview := Interview{}
 	copier.Copy(&dbInterview, interviewRequest)
@@ -128,25 +103,4 @@ func QueryAllInterviewOfRound(eid uint, did uint, rnd uint) (*[]Brief, error) {
 	}
 	result := gormDb.Model(&Interview{}).Where(&Interview{EventID: eid, DepartmentID: did, Round: rnd}).Find(&dbInterview)
 	return &dbInterview, result.Error
-}
-
-func UpdateJoinedInterview(id uint, newResult uint) error {
-	result := gormDb.Model(&JoinedInterview{ID: id}).Update("result", newResult)
-	return result.Error
-}
-
-func QueryAllJoinedInterviewOfInterview(iid uint) (*[]JoinedInterview, error) {
-	var dbJoinedInterview []JoinedInterview
-	result := gormDb.Model(&JoinedInterview{}).Where(&JoinedInterview{InterviewID: iid}).Find(&dbJoinedInterview)
-	return &dbJoinedInterview, result.Error
-}
-
-func CreateInterviewee(interviewee *Interviewee) (uint, error) {
-	result := gormDb.Create(interviewee)
-	return interviewee.ID, result.Error
-}
-
-func UpdateInterviewee(interviewee *Interviewee, vid uint) error {
-	result := gormDb.Model(&Interviewee{ID: vid}).Updates(interviewee)
-	return result.Error
 }
---
+++ model/interviewee.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/datatypes"
+)
+
+// 一个志愿一条记录
+type Interviewee struct {
+	ID               uint           `gorm:"not null;autoIncrement;primaryKey"`
+	EventID          uint           `gorm:"not null"`
+	AnswerID         uint           `gorm:"not null"`
+	DepartmentID     uint           `gorm:"not null"`           // 志愿部门
+	IntentRank       uint           `gorm:"not null;default:0"` // 第几志愿
+	Round            uint           `gorm:"not null;default:1"` // 公海为1，一面为2，以此类推
+	SentMessage      uint           `gorm:"not null;default:1"` // 发送过选择面试场次短信的为2，没有为1
+	InterviewOptions datatypes.JSON // 发送选择面试场次的短信用
+	Status           uint           `gorm:"not null; default:1"` // 1 本轮接受但还没选择下轮面试时间，2 面试进行中，3 纳入组织，4 拒绝
+}
+
+type IntervieweeRequest struct {
+	InterviewOptions []uint `json:"InterviewOptions"`
+}
+
+type JoinedInterview struct {
+	ID            uint      `gorm:"not null;autoIncrement;primaryKey"`
+	InterviewID   uint      `gorm:"not null"`
+	IntervieweeID uint      `gorm:"not null"`
+	Result        uint      `gorm:"default:0"`
+	UpdatedTime   time.Time `gorm:"not null"`
+}
+
+func CreateInterviewee(interviewee *Interviewee) (uint, error) {
+	result := gormDb.Create(interviewee)
+	return interviewee.ID, result.Error
+}
+
+func UpdateInterviewee(interviewee *Interviewee, vid uint) error {
+	result := gormDb.Model(&Interviewee{ID: vid}).Updates(interviewee)
+	return result.Error
+}
+
+func UpdateJoinedInterview(id uint, newResult uint) error {
+	result := gormDb.Model(&JoinedInterview{ID: id}).Update("result", newResult)
+	return result.Error
+}
+
+func QueryAllJoinedInterviewOfInterview(iid uint) (*[]JoinedInterview, error) {
+	var dbJoinedInterview []JoinedInterview
+	result := gormDb.Model(&JoinedInterview{}).Where(&JoinedInterview{InterviewID: iid}).Find(&dbJoinedInterview)
+	return &dbJoinedInterview, result.Error
+}
